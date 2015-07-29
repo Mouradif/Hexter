29ada16ec0f9c4e22d1b77b4f70dbc907352acd1
Jeremie LAWSON <jlawson@student.42.fr>
2015-07-29 20:21:09 +0200

    Ajout des prototypes de fonctions dans bsq.h

--- include/bsq.h
+++ include/bsq.h
@@ -6,7 +6,7 @@
 /*   By: jlawson <jlawson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/27 16:38:51 by jlawson           #+#    #+#             */
-/*   Updated: 2015/07/29 18:28:59 by mkejji           ###   ########.fr       */
+/*   Updated: 2015/07/29 20:19:23 by jlawson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define BSQ_H
 # include "struct.h"
 
+int				ft_allowed_chars(t_grid *grid, char c);
+char			*read_file(char *path);
+char			*ft_read(int fd);
+void			ft_cell_increment(t_grid *grid, size_t i, size_t j);
+void			ft_remap(t_grid *grid);
+int				ft_cell_search(t_grid grid, size_t i, size_t j);
+void			ft_set_params(t_grid *grid);
 char			*ft_realloc(char *buf, size_t size);
 int				is_printable(char c);
 int				errno(int err);
