ea1f7c2db32c30e2776b1d8a4684bc4f7e087afb
Jeremie LAWSON <jlawson@student.42.fr>
2015-07-29 19:57:58 +0200

    Changement des errno, renommage de ft_remap en grid.

--- src/grid.c
+++ src/grid.c
@@ -5,45 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlawson <jlawson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/07/28 15:27:00 by jlawson           #+#    #+#             */
-/*   Updated: 2015/07/29 08:59:33 by mkejji           ###   ########.fr       */
+/*   Created: 2015/07/29 19:44:15 by jlawson           #+#    #+#             */
+/*   Updated: 2015/07/29 19:44:17 by jlawson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "struct.h"
-#include "bsq.h"
 
-t_grid		*get_grid(char *str)
+int		ft_cell_search(t_grid grid, size_t i, size_t j)
 {
-	t_grid *grid;
+	char r;
+	char dr;
+	char d;
+	char o;
 
-	grid = get_param(str);
-	return (grid);
+	o = grid.obs;
+	if (i >= (grid.width - 1) || j >= (grid.height - 1))
+		return (0);
+	else
+	{
+		r = grid.grid[j][i + 1].cell;
+		dr = grid.grid[j + 1][i + 1].cell;
+		d = grid.grid[j + 1][i].cell;
+		if (r == o || dr == o || d == o)
+			return (0);
+		else
+			return (1);
+	}
 }
 
-int			fill_grid(t_grid *grid)
+void	ft_cell_increment(t_grid *grid, size_t i, size_t j)
 {
-	char	*str;
-	size_t	i;
-	size_t	j;
+	char ul;
+	char o;
 
-	str = grid->str;
+	o = grid->obs;
+	grid->grid[j][i].index += 1;
+	if (i > 0 && j > 0)
+	{
+		ul = grid->grid[i - 1][j - 1].cell;
+		if (ul != o)
+			ft_cell_increment(grid, (i - 1), (j - 1));
+	}
+}
+
+void	ft_remap(t_grid *grid)
+{
+	size_t i;
+	size_t j;
+
 	j = 0;
 	while (j < grid->height)
 	{
 		i = 0;
 		while (i < grid->width)
 		{
-			if (*str == '\n' || *str == '\0')
-				return (errno(3));
-			grid->grid[j][i].cell = *str;
-			str++;
-			i++;
+			if (ft_cell_search(*grid, i, j))
+				ft_cell_increment(grid, i, j);
 		}
-		if (*str != '\n')
-			return (errno(3));
-		str++;
-		j++;
 	}
-	return (1);
 }
