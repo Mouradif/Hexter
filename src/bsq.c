091fd8c0453a01fa269adde2dc19c4eb02828b9e
Mourad KEJJI <mkejji@gmail.com>
2015-07-29 19:14:27 +0200

    nettoyage des srcs

--- src/bsq.c
+++ src/bsq.c
@@ -6,25 +6,6 @@
 /*   By: jlawson <jlawson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/27 18:37:59 by jlawson           #+#    #+#             */
-/*   Updated: 2015/07/29 18:10:09 by jlawson          ###   ########.fr       */
+/*   Updated: 2015/07/29 19:13:22 by mkejji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-#include "ft.h"
-#include "struct.h"
-#include "bsq.h"
-
-unsigned int	ft_count_lines(char *str)
-{
-	unsigned int ret;
-
-	ret = 0;
-	while (*str)
-	{
-		if (*str == '\n')
-			ret++;
-		str++;
-	}
-	return (ret);
-}
