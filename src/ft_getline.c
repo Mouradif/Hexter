7098144176a8d245b806f3dcdb736294ac6a3c98
Jeremie LAWSON <jlawson@student.42.fr>
2015-07-30 16:55:03 +0200

    Initialisation de la variable i en dehors de la boucle

--- src/ft_getline.c
+++ src/ft_getline.c
@@ -6,7 +6,7 @@
 /*   By: jlawson <jlawson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/30 12:59:20 by jlawson           #+#    #+#             */
-/*   Updated: 2015/07/30 16:51:30 by jlawson          ###   ########.fr       */
+/*   Updated: 2015/07/30 16:53:01 by jlawson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,10 @@ int		ft_getline(char **buf, int fd)
 
 	size = BUFFER_SIZE;
 	*buf = malloc(sizeof(char) * size + 1);
+	i = 0;
 	if (*buf)
 	{
 		ft_memset(*buf, size + 1);
-		i = 0;
 		count = read(fd, (*buf + i), 1);
 		while (count > 0 && (*buf)[i] && (*buf)[i] != '\n')
 		{
